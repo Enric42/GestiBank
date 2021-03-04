f81b763ef7a9427e67283af724793875c6deb51a
Reiler74 <alexfontainevr@yahoo.fr>
2021-03-04 11:25:28 +0100

    modif compte beneficiaire

--- src/com/wha/entities/CompteBeneficiaire.java
+++ src/com/wha/entities/CompteBeneficiaire.java
@@ -4,6 +4,19 @@ public class CompteBeneficiaire implements ICompte {
 
 	private String libelle;
 	private String rib;
+	private Boolean proprietaire;
+	
+	
+	public CompteBeneficiaire (String libelle, String rib) {
+		this.setLibelle(libelle);
+		this.setRib(rib);
+		this.setProprietaire(proprietaire);
+	}
+	
+	public CompteBeneficiaire () {
+	}
+
+	
 	
 	// Getters & Setters
 
@@ -22,4 +35,13 @@ public class CompteBeneficiaire implements ICompte {
 	public void setRib(String rib) {
 		this.rib = rib;
 	}
+	
+	public Boolean getProprietaire() {
+		return proprietaire;
+	}
+	
+	public void setProprietaire(Boolean proprietaire) {
+		this.proprietaire = proprietaire;
+	}
 }
+
